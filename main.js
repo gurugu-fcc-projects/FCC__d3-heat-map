423fafe7294ffc6a5859c8f127c71dd0e9931165
GuRuGu <peter.k.nets@gmail.com>
2020-04-15 10:10:47 +0300

    Move main title into header

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const url =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 
+// Main elements
+
 d3.json(url)
   .then((data) => {
     console.log(data);

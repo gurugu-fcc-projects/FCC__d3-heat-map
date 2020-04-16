8b9451103c6edae7d015303891ddce00f78fae85
GuRuGu <peter.k.nets@gmail.com>
2020-04-16 15:03:03 +0300

    Add preliminary y-Axis

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+// Main data
 const url =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 
-// Main elements
+// Dimensions
+const margin = { top: 20, right: 20, bottom: 100, left: 70 };
+const height = 600 - margin.top - margin.bottom;
+const width = 1000 - margin.left - margin.right;
+
+// SVG & Chart
+const svg = d3.select(".content").append("svg");
+const chart = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Y Scale & Axis
+const yScale = d3.scaleLinear().domain([0, 11]).range([height, 0]);
+const yAxis = d3.axisLeft(yScale);
+chart.append("g").call(yAxis);
 
 d3.json(url)
   .then((data) => {

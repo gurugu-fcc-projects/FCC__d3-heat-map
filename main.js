49e4c90c27e5c524373c787dc2d5872135f09e80
GuRuGu <peter.k.nets@gmail.com>
2020-04-23 11:27:11 +0300

    Display months on Y axis

--- main.js
+++ main.js
@@ -6,16 +6,36 @@ const url =
 const margin = { top: 20, right: 20, bottom: 100, left: 70 };
 const height = 600 - margin.top - margin.bottom;
 const width = 1000 - margin.left - margin.right;
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 // SVG & Chart
-const svg = d3.select(".content").append("svg");
+const svg = d3
+  .select(".content")
+  .append("svg")
+  .attr("height", height + margin.top + margin.bottom)
+  // .attr("height", "100vh")
+  .attr("width", width + margin.left + margin.right);
+// .attr("width", "100%");
 const chart = svg
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Y Scale & Axis
 const yScale = d3.scaleLinear().domain([0, 11]).range([height, 0]);
-const yAxis = d3.axisLeft(yScale);
+const yAxis = d3.axisLeft(yScale).tickFormat((d) => months[d - 1]);
 chart.append("g").call(yAxis);
 
 d3.json(url)

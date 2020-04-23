2bc01a7289d5ce196c39da1263328e044edf7681
GuRuGu <peter.k.nets@gmail.com>
2020-04-23 12:49:24 +0300

    Display data cells

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const url =
 const margin = { top: 20, right: 20, bottom: 100, left: 70 };
 const height = 600 - margin.top - margin.bottom;
 const width = 1000 - margin.left - margin.right;
-const cellSize = 30;
+const cellSize = 20;
 
 // Helpers
 const months = [
@@ -29,16 +29,18 @@ const svg = d3
   .select(".content")
   .append("svg")
   .attr("height", height + margin.top + margin.bottom)
-  // .attr("height", "100vh")
   .attr("width", width + margin.left + margin.right);
-// .attr("width", "100%");
+
 const chart = svg
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// Load & display data
 d3.json(url)
   .then((data) => {
     console.log(data);
+    const minYear = d3.min(data.monthlyVariance, (d) => d.year);
+    console.log(minYear);
 
     // Y Scale & Axis
     const yScale = d3
@@ -48,7 +50,7 @@ d3.json(url)
 
     const yAxis = d3.axisLeft(yScale).tickFormat((d) => months[d - 1]);
 
-    chart.append("g").call(yAxis);
+    // chart.append("g").call(yAxis);
 
     // X Scale & Axis
     const xScale = d3
@@ -58,12 +60,26 @@ d3.json(url)
 
     const xAxis = d3.axisBottom(xScale).tickFormat((d) => String(d));
 
+    // chart
+    //   .append("g")
+    //   .call(xAxis)
+    //   .attr(
+    //     "transform",
+    //     `translate(0, ${height - margin.top - margin.bottom})`
+    //   );
+
+    // Data cells
     chart
-      .append("g")
-      .call(xAxis)
-      .attr(
-        "transform",
-        `translate(0, ${height - margin.top - margin.bottom})`
-      );
+      .selectAll("rect")
+      .data(data.monthlyVariance)
+      .enter()
+      .append("rect")
+      .attr("width", cellSize)
+      .attr("height", cellSize)
+      .attr("x", (d) => (d.year - minYear) * cellSize)
+      .attr("y", (d) => (d.month - 1) * cellSize)
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .style("fill", "tomato");
   })
   .catch((error) => console.log(error));
